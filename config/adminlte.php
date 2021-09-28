390c5b91c3e5d856af72c4dd05631070006f025e
Andres Silva Alvarez <jandresi@hotmail.com>
2021-09-27 21:56:08 -0500

    Sidebar configurado

--- config/adminlte.php
+++ config/adminlte.php
@@ -46,11 +46,11 @@ return [
     */
 
     'logo' => '<b>Gestión de</b> Activos',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => 'vendor/adminlte/dist/img/cloud.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'Silva y Joan',
 
     /*
     |--------------------------------------------------------------------------
@@ -85,8 +85,8 @@ return [
 
     'layout_topnav' => null,
     'layout_boxed' => null,
-    'layout_fixed_sidebar' => null,
-    'layout_fixed_navbar' => null,
+    'layout_fixed_sidebar' => true,
+    'layout_fixed_navbar' => true,
     'layout_fixed_footer' => null,
     'layout_dark_mode' => null,
 
@@ -102,12 +102,12 @@ return [
     |
     */
 
-    'classes_auth_card' => 'bg-gradient-dark',
+    'classes_auth_card' => 'card-outline card-danger bg-gradient-dark',
     'classes_auth_header' => '',
     'classes_auth_body' => 'bg-gradient-dark',
-    'classes_auth_footer' => 'text-center',
+    'classes_auth_footer' => 'd-none',
     'classes_auth_icon' => 'text-light',
-    'classes_auth_btn' => 'btn-flat btn-light',
+    'classes_auth_btn' => 'btn-danger',
 
     /*
     |--------------------------------------------------------------------------
@@ -122,14 +122,14 @@ return [
     */
 
     'classes_body' => '',
-    'classes_brand' => '',
+    'classes_brand' => 'bg-dark',
     'classes_brand_text' => '',
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
     'classes_sidebar' => 'sidebar-dark-primary elevation-4',
     'classes_sidebar_nav' => '',
-    'classes_topnav' => 'navbar-white navbar-light',
+    'classes_topnav' => 'navbar-dark navbar-light',
     'classes_topnav_nav' => 'navbar-expand',
     'classes_topnav_container' => 'container',
 
@@ -148,7 +148,7 @@ return [
     'sidebar_mini' => 'lg',
     'sidebar_collapse' => false,
     'sidebar_collapse_auto_size' => false,
-    'sidebar_collapse_remember' => false,
+    'sidebar_collapse_remember' => true,
     'sidebar_collapse_remember_no_transition' => true,
     'sidebar_scrollbar_theme' => 'os-theme-light',
     'sidebar_scrollbar_auto_hide' => 'l',
@@ -188,7 +188,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => '',
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
@@ -233,7 +233,7 @@ return [
         ],
         [
             'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
+            'topnav_right' => false,
         ],
 
         // Sidebar items:
@@ -247,14 +247,40 @@ return [
             'url'  => 'admin/blog',
             'can'  => 'manage-blog',
         ],
+        ['header' => 'PESTAÑAS DE GESTIÓN'],
         [
-            'text'        => 'Activos',
+            'text'        => 'Lista De Activos',
             'url'         => 'activos',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 'CRUD',
-            'label_color' => 'success',
+            'icon'        => 'fas fa-fw fa-tools',
+            //'label'       => '',
+            //'label_color' => 'success',
+            'icon_color' => 'white',
         ],
-        ['header' => 'Configuración de cuenta'],
+        [
+            'text'        => 'Entregar Pedido',
+            'url'         => 'entregas',
+            'icon'        => 'fas fa-fw fa-scroll',
+            //'label'       => '',
+            //'label_color' => 'success',
+            'icon_color' => 'white',
+        ],
+        [
+            'text'        => 'Crear Receptor',
+            'url'         => 'receptores',
+            'icon'        => 'fas fa-fw fa-user-edit',
+            //'label'       => '',
+            //'label_color' => 'success',
+            'icon_color' => 'white',
+        ],
+        [
+            'text'        => 'Ver Reportes',
+            'url'         => 'reportes',
+            'icon'        => 'fas fa-fw fa-chart-line',
+            //'label'       => '',
+            //'label_color' => 'success',
+            'icon_color' => 'white',
+        ],
+        /* ['header' => 'Configuración de cuenta'],
         [
             'text' => 'Perfil de usuario',
             'url'  => 'user/profile',
@@ -302,23 +328,7 @@ return [
                     'url'  => '#',
                 ],
             ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+        ], */
     ],
 
     /*
